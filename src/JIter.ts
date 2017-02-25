a48eeab84a739e2d79e3ee9153c9503d54072146
Chris van der Pennen <chrisvanderpennen@users.noreply.github.com>
2017-02-25 21:32:50 +1030

    Add skip/skipWhile, take/takeWhile, orderBy/thenBy

--- src/JIter.ts
+++ src/JIter.ts
@@ -1,11 +1,11 @@
 export class JIter<T> implements Iterable<T> {
-    private source: Iterable<T>;
+    protected source: Iterable<T>;
 
     public static create<T>(iterable: Iterable<T>): JIter<T> {
         return new JIter<T>(iterable);
     }
 
-    private constructor(source: Iterable<T>) {
+    protected constructor(source: Iterable<T>) {
         this.source = source;
     }
 
@@ -29,6 +29,26 @@ export class JIter<T> implements Iterable<T> {
         return new JIter(distinct(this.source));
     }
 
+    public skip(count: number): JIter<T> {
+        return new JIter(skip(this.source, count));
+    }
+
+    public skipWhile(whileFn: (item: T) => boolean): JIter<T> {
+        return new JIter(skipWhile(this.source, whileFn));
+    }
+
+    public take(count: number): JIter<T> {
+        return new JIter(take(this.source, count));
+    }
+
+    public takeWhile(whileFn: (item: T) => boolean): JIter<T> {
+        return new JIter(takeWhile(this.source, whileFn));
+    }
+
+    public orderBy(compareFn: CompareFn<T>): JOrderingIter<T> {
+        return new JOrderingIter<T>(this.source, [compareFn]);
+    }
+
     public join<TOuter, TKey, TResult>(
         outer: Iterable<TOuter>,
         innerKeyFn: (item: T) => TKey,
@@ -42,6 +62,38 @@ export class JIter<T> implements Iterable<T> {
     // tslint:disable-next-line:function-name
     public [Symbol.iterator]() {
         return this.source[Symbol.iterator]();
+    }
+}
+
+type CompareFn<T> = (a: T, b: T) => number;
+
+class JOrderingIter<T> extends JIter<T> {
+    private comparators: CompareFn<T>[];
+
+    constructor(source: Iterable<T>, comparators: CompareFn<T>[]) {
+        super(source);
+        this.comparators = comparators;
+    }
+
+    public thenBy(comparator: CompareFn<T>) {
+        return new JOrderingIter(this.source, [...this.comparators, comparator]);
+    }
+
+    // This needs to be Symbol.iterator()
+    // tslint:disable-next-line:function-name
+    public *[Symbol.iterator]() {
+        const list = Array.from(this.source);
+        list.sort((a, b) => {
+            let result = 0;
+            for (const comparator of this.comparators) {
+                result = comparator(a, b);
+                if (result !== 0) {
+                    break;
+                }
+            }
+            return result;
+        });
+        yield *list;
     }
 }
 
@@ -103,5 +155,53 @@ function *distinct<T>(source: Iterable<T>) {
         }
         $set.add(item);
         yield item;
+    }
+}
+
+function *take<T>(source: Iterable<T>, count: number): Iterable<T> {
+    const iterator = source[Symbol.iterator]();
+    for (let i = 0; i < count; i += 1) {
+        const item = iterator.next();
+        if (item.done) {
+            return;
+        }
+        yield item.value;
+    }
+}
+
+function *takeWhile<T>(source: Iterable<T>, whileFn: (item: T) => boolean): Iterable<T> {
+    for (const item of source) {
+        if (whileFn(item)) {
+            yield item;
+        } else {
+            return;
+        }
+    }
+}
+
+function *skip<T>(source: Iterable<T>, count: number): Iterable<T> {
+    const iterator = source[Symbol.iterator]();
+    for (let i = 0; i < count; i += 1) {
+        const item = iterator.next();
+        if (item.done) {
+            return;
+        }
+    }
+    let item = iterator.next();
+    while (!item.done) {
+        yield item.value;
+        item = iterator.next();
+    }
+}
+
+function *skipWhile<T>(source: Iterable<T>, whileFn: (item: T) => boolean): Iterable<T> {
+    let skipping = true;
+    for (const item of source) {
+        if (skipping && whileFn(item)) {
+            continue;
+        } else {
+            skipping = false;
+            yield item;
+        }
     }
 }
